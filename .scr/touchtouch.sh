481eb40fa8fe199129290adaf8eeeed86ff74e9c
Sofee <sofeeys@outlook.com>
2018-08-06 23:41:20 +0800

    Add: script for touchpad

---
+++ .scr/touchtouch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+s=$(echo $(synclient | grep -i tapbutton1) | sed -e 's/^.*\(.\)$/\1/g')
+
+if [ $s = 0 ]; then
+    synclient TapButton1=1
+    synclient TapButton2=3
+    synclient TapButton3=2
+else
+    synclient TapButton1=0
+    synclient TapButton2=0
+    synclient TapButton3=0
+fi
+
